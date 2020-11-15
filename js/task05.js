deff673e739776034ba31d112974ebd956587dfd
Osipov Sergey <smozoluk@gmail.com>
2020-11-15 23:15:39 +0200

    tasks 5-7

--- js/task05.js
+++ js/task05.js
@@ -1,6 +1,7 @@
 const nameInputRef = document.getElementById("name-input");
+const nameOutput = document.getElementById("name-output");
 
-nameInputRef.addEventListener("klick", function() {
-    console.log(1)
+nameInputRef.addEventListener("input", function () {
+    
+    nameOutput.textContent = nameInputRef.value;
 })
-
---
+++ js/task06.js
@@ -0,0 +1,22 @@
+const inputRef = document.getElementById("validation-input");
+
+inputRef.addEventListener("blur", function () {
+    const inputLength = Number(inputRef.value.length);
+    const correctLength = Number(inputRef.dataset.length);
+    inputLength === correctLength
+        ? inputRef.classList.remove("invalid") || inputRef.classList.add("valid")
+        : inputRef.classList.remove("valid") || inputRef.classList.add("invalid");
+})
+
+// let input = document.querySelector('input')
+
+// input.addEventListener('blur', function() {
+//   let size = this.getAttribute('data-size');
+//   let len = this.value.length
+//   let currentClass = (len = size) ? 'valid' : 'invalid'
+//   toggleClass(this, currentClass )
+// })
+// function toggleClass(elem, currentClass) {
+//   elem.className = ''
+//   elem.classList.add(currentClass)
+// }
